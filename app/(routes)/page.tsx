9599319a69dea4b20d3a4a6a592b0c7f31ed3b02
= <=>
2023-08-28 23:50:50 -0500

    +landing +category +product +filters +sizes +cart

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,26 @@
+import { getBillboards } from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboards = await getBillboards();
+  console.log(billboards);
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboards[0]} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
